8a1d8b9452c5dc1aa2370e9d3ef8f140ae5f218e
sammlapp <sammlapp@gmail.com>
2025-11-11 17:02:39 -0500

    remove mismatched border-radius

--- frontend/src/components/FocusView.js
+++ frontend/src/components/FocusView.js
@@ -407,7 +407,6 @@ function FocusView({
                       backgroundColor: ((option.value === 'unlabeled' && annotationValue === null) ||
                         (option.value !== 'unlabeled' && annotationValue === option.value)) ? option.color : 'transparent',
                       border: "none",
-                      borderRadius: "10px",
                       fontSize: '1rem',
                       color: ((option.value === 'unlabeled' && annotationValue === null) ||
                         (option.value !== 'unlabeled' && annotationValue === option.value)) ? 'white' : option.color
